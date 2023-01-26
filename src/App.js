4e09a247c926ee0b54624baeee2ac9fe0580f551
ea <aboajah.emmanuel@fmdqgroup.com>
2023-01-26 17:20:53 +0100

    dashboard

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-import "./app.css";
+import "./App.css";
 import Home from "./components/pages/home/Home";
 import UserList from './components/pages/userList/UserList';
 import User from "./components/pages/user/User";
